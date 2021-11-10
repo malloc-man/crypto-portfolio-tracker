fbc97d7db06a19ff780a8aa196752f6e9b72fbb0
Jonathan <jphillips599@gmail.com>
2021-11-10 09:53:52 -0500

    Refactored to use wallet classes and objects. Added support for SOL.

--- tracker.py
+++ tracker.py
@@ -4,91 +4,138 @@ def main():
     wallets = getWallets()
     total = 0
     for wallet in wallets:
-        if 'LUNA' in wallet.keys():
-            total += getTerraWalletValue(wallet['LUNA'])
+        total += wallet.getWalletValue()
     return formatUsd(total)
 
-# Gets coin names and wallet addresses from user.
-# Returns a list of dictionaries. Each dictionary consists of a single key-value pair.
-# The key is a string representing the coin name (e.g. LUNA).
-# The value is the wallet address.
+class Wallet:
+    def __init__(self, address):
+        self.address = address
+    
+    def getWalletValue(self):
+        self.getWalletValue()
+
+class LunaWallet(Wallet):
+    pricingDataSource = "https://fcd.terra.dev/v1/market/swaprate/uusd"
+    divFactor = 1000000
+
+    def __init__(self, address):
+        super().__init__(address)
+        self.balanceDataSource = "https://fcd.terra.dev/v1/bank/" + self.address
+        self.coins = self.getCoinSet()
+    
+    def getLiquidBalances(self):
+        return requests.get(self.balanceDataSource).json()["balance"]
+    
+    def getStakedBalances(self):
+        return requests.get(self.balanceDataSource).json()["delegations"]
+    
+    def getCoinSet(self):
+        coinSet = set()
+        liq = self.getLiquidBalances()
+        for item in liq:
+            if 'denom' in item.keys():
+                coinSet.add(item['denom'])
+        return coinSet
+    
+    def getCoinBalance(self, coin):
+        bal = 0
+        liq = self.getLiquidBalances()
+        stk = self.getStakedBalances()
+        for item in liq:
+            if item['denom'] == coin:
+                bal += float(item['available'])
+                break
+        if coin == 'uluna':
+            for item in stk:
+                bal += float(item['amount'])
+        return bal / self.divFactor
+
+    def getCoinBalanceCached(self, coin, liq_cache, stk_cache):
+        bal = 0
+        for item in liq_cache:
+            if item['denom'] == coin:
+                bal += float(item['available'])
+                break
+        if coin == 'uluna':
+            for item in stk_cache:
+                bal += float(item['amount'])
+        return bal / self.divFactor
+
+    def getPrices(self):
+        prices = {}
+        datastream = requests.get(self.pricingDataSource).json()
+        for item in datastream:
+            prices[item['denom']] = 1 / float(item['swaprate'])
+        return prices
+
+    def getCoinPrice(self, coin):
+        return self.getPrices()[coin]
+    
+    def getCoinPriceCached(self, coin, cache):
+        return cache[coin]
+        
+    def getWalletValue(self):
+        total = 0
+        prices = self.getPrices()
+        liq = self.getLiquidBalances()
+        stk = self.getStakedBalances()
+        for coin in self.coins:
+            if coin != 'uusd' and coin != 'ibc/0471F1C4E7AFD3F07702BEF6DC365268D64570F7C1FDC98EA6098DD6DE59817B':
+                price = self.getCoinPriceCached(coin, prices)
+                quantity = self.getCoinBalanceCached(coin, liq, stk)
+                total += price * quantity
+        return total
+    
+class SolWallet(Wallet):
+    coins = set("SOL")
+    divFactor = 1000000000
+
+    def __init__(self, address):
+        super().__init__(address)
+        self.balanceDataSource = "https://prod-api.solana.surf/v1/account/" + self.address
+        self.stakingDataSource = "https://prod-api.solana.surf/v1/account/" + self.address + "/stakes?limit=10&offset=0"
+    
+    def getLiquidBalances(self):
+        return {'SOL': requests.get("https://prod-api.solana.surf/v1/account/" + self.address).json()['value']['base']['balance'] / self.divFactor}
+
+    def getStakedBalances(self):
+        response = requests.get("https://prod-api.solana.surf/v1/account/" + self.address + "/stakes?limit=10&offset=0").json()
+        total = 0
+        for dict in response['data']:
+            total += dict['lamports']
+        return {'SOL': total / self.divFactor}
+
+    def getPrice(self):
+        return requests.get("https://api.coingecko.com/api/v3/coins/solana").json()["market_data"]["current_price"]["usd"]
+    
+    def getWalletValue(self):
+        price = self.getPrice()
+        quantity = self.getLiquidBalances()['SOL'] + self.getStakedBalances()['SOL']
+        return price * quantity
+
+# Gets coin names and wallet addresses from user. Returns a list of wallet objects.
 def getWallets():
     lst = []
     while True:
-        coin = input("Please input the coin this wallet contains (e.g. BTC, ETH): ") # only works for LUNA right now
+        coin = input("Please input the coin this wallet contains (LUNA or SOL): ")
         s = input("Please input your public wallet address: ")
-        lst.append({coin.strip().upper(): s.strip()})
-        anotherWallet = input("Another wallet? y/n: ")
-        if anotherWallet == "n" or anotherWallet == "N":
+        coin = coin.strip().upper()
+        s = s.strip()
+        if coin == 'LUNA':
+            lst.append(LunaWallet(s))
+        elif coin == 'SOL':
+            lst.append(SolWallet(s))
+        done = False
+        while True:
+            anotherWallet = input("Another wallet? y/n: ")
+            if anotherWallet == "y" or anotherWallet == "Y":
+                break
+            if anotherWallet == "n" or anotherWallet == "N":
+                done = True
+                break
+        if done:
             break
     return lst
-
-# Takes in a string representing a Terra wallet address 
-# and returns the value of the coins held by that wallet.
-def getTerraWalletValue(wallet):
-    wallet_datastream = getTerraWalletData(wallet)
-    swaprates = constructSwapRateDict()
-
-    retrieved_balances = wallet_datastream["balance"]
-    retrieved_delegations = wallet_datastream["delegations"]
-
-    balances = constructWalletBalanceDict(retrieved_balances, retrieved_delegations)
-
-    return calculateSum(balances, swaprates)
-
-# Queries Terra data source for balances of coins in wallet.
-# Returns a dictionary containing four keys: balance, vesting, delegations, and unbonding.
-def getTerraWalletData(wallet):
-    response = requests.get("https://fcd.terra.dev/v1/bank/" + wallet)
-    return response.json()
-
-# Queries Terra market data for price of coins denominated in USD (UST).
-# Returns a list of dictionaries, one for each coin.
-# Each coin's dictionary has the keys 'denom', 'swaprate', 'oneDayVariation', and 'oneDayVariationRate'.
-# The most relevant ones here are 'denom' (the name of the coin) and 'swaprate' (how much of that coin to buy 1 UST).
-def getTerraMarketData():
-    return requests.get("https://fcd.terra.dev/v1/market/swaprate/uusd").json()
-
-# Returns a dictionary whose keys are the names of coins in the Terra swap market
-# and whose values are the swap rates for that coin (how much of that coin to buy 1 UST).
-def constructSwapRateDict():
-    swaprates = {}
-    datastream = getTerraMarketData()
-    for item in datastream:
-        swaprates[item['denom']] = item['swaprate']
-    return swaprates
-
-# Takes in as parameters the name of a coin and the swaprate dictionary
-# and returns the swap rate for that coin.
-def getSwapRate(coin, swaprates):
-    for item in swaprates:
-        if item == coin:
-            return float(swaprates[item])
-
-# Returns a dictionary whose keys are the names of coins owned by the wallet
-# and whose values are the number of such coins in the wallet.
-def constructWalletBalanceDict(retrieved_balances, retrieved_delegations):
-    balances = {}
-    for item in retrieved_balances:
-        if item['denom'] != 'ibc/0471F1C4E7AFD3F07702BEF6DC365268D64570F7C1FDC98EA6098DD6DE59817B':
-            balances[item['denom']] = round(float(item['available']) / 1000000, 6)
-        if item['denom'] == 'uluna':
-            luna = float(item['available']) / 1000000
-    for item in retrieved_delegations:
-        luna += float(item['amount']) / 1000000
-    balances['uluna'] = round(luna, 6)
-    return balances
-
-# Takes in as parameters the balances dictionary (which lists how much of each coin the wallet has)
-# and the swaprates dictionary (which gives how much of each coin is required to buy 1 UST)
-# and returns the value in UST of the wallet's holdings.
-def calculateSum(balances, swaprates):
-    sum = 0
-    for item in balances:
-        if item != 'uusd':
-            rate = getSwapRate(item, swaprates)
-            sum += balances[item] / rate
-    return sum
 
 # Formats a numerical sum as a USD string (e.g. $x,xxx.xx).
 def formatUsd(amount):
